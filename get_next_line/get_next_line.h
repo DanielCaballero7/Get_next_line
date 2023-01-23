37ca3386172023d26493364f33ad30804c93867a
Daniel Caballero Alonso <dcaballe@student.42malaga.com>
2023-01-23 19:29:43 +0100

    get_next_line v1

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaballe <dcaballe@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 15:38:00 by dcaballe          #+#    #+#             */
+/*   Updated: 2022/09/20 15:50:52 by dcaballe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h> 
+# include <stdlib.h>
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_extract_line(char *fd_read);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_delete_line(char *fd_read);
+int		ft_check_bad_parameters(int fd, char *fd_read);
+char	*ft_readfd(int fd, char *fd_read);
+#endif
